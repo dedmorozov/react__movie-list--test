0e910f390ad07218105e0d3db30c5c6824facc2b
Dmitry Morozov <10mdv11@gmail.com>
2020-11-14 15:33:33 +0200

    done

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,4 @@
 // REACT
 module.exports = {
-  "extends": "@mate-academy/eslint-config-react"
-}
-
-// REACT TYPESCRIPT
-module.exports = {
-  "extends": "@mate-academy/eslint-config-react-typescript"
-}
+  extends: '@mate-academy/eslint-config-react',
+};
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,48 +1,54 @@
-import React, { PureComponent } from 'react';
-// import PropTypes from 'prop-types';
-import { MovieCard } from '../MovieCard/MovieCard';
+/* eslint-disable no-underscore-dangle */
 
-import { getApi } from '../api'
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { MovieCard } from '../MovieCard/MovieCard';
 
 export class MovieList extends PureComponent {
   state = {
-    shows: [],
-  }
 
-  componentDidMount() {
-    getApi('/schedule').then(result => this.setState({shows: result}))
   }
 
   render() {
-    const { shows } = this.state;
+    const { preparedShows = [] } = this.props;
 
     return (
-      // <>
-        <div className="content">
-          <ul className="content__list list">
-            {shows.map(item => (
-              <li key={item.show.id} className="list__item">
-                <MovieCard
-                  title={item.show.name}
-                  year={(item.show.premiered).split('', 4)}
-                  // imgUrl={item.show.image.original}
-                  imgUrl={"http://static.tvmaze.com/uploads/images/medium_portrait/214/535400.jpg"}
-                />
-              </li>
-            ))}
-          </ul>
-        </div>
-      // </>
-    )
+      <div className="content">
+        <ul className="content__list list">
+          {preparedShows.map(item => (
+            <li key={item.id} className="list__item">
+              <MovieCard
+                title={item._embedded.show.name}
+                year={(item._embedded.show.premiered)
+                  .split('', 4)
+                  .join('')}
+                imgUrl={
+                  item._embedded.show.image !== null
+                    ? item._embedded.show.image.medium
+                    : ''
+                }
+                episode={`Сезон: ${item.season} Эпизод: ${item.number}`}
+                openInNewTab={
+                  item._embedded.show.image !== null
+                    // eslint-disable-next-line
+                    ? () => window.open(
+                      item._embedded.show.image.original,
+                      '_blank', 'noopener, noreferrer',
+                    )
+                    // eslint-disable-next-line
+                    : () =>  window.open(`https://google.com/search?q=${item._embedded.show.name} series pictures`,
+                      '_blank', 'noopener, noreferrer')
+                }
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
   }
-  
 }
 
-// MovieList.propTypes = {
-//   selectedDay: PropTypes.number.isRequired,
-//   isSelected: PropTypes.bool,
-// }
-
-// MovieList.defaultProps = {
-//   isSelected: false,
-// }
+MovieList.propTypes = {
+  /* eslint-disable-next-line */
+  preparedShows: PropTypes.array.isRequired,
+};
